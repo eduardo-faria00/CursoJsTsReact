d2d077013a799e853df091fa4a090f4c97fefa0e
Izael Jesus <avspnn@gmail.com>
2022-07-13 08:35:39 -0300

    primeiro commit

---
+++ aula10/index.js
@@ -0,0 +1,15 @@
+let nome = 'Duardo'
+let sobrenome = 'Faria'
+
+nome+=sobrenome
+
+console.log(nome)
+
+nome=sobrenome
+
+console.log(nome)
+
+/* 
+O operador += une os valores de nome e sobrenome,
+enquanto que o operador = atribui à nome o mesmo valor de sobrenome
+*/
---
+++ aula11/main.js
@@ -0,0 +1,11 @@
+let n1 = prompt('digite  número')
+let n2 = prompt('digite outro número')
+
+n1 = Number(n1)
+n2 = Number(n2)
+const soma = n1+n2
+
+
+alert(`A soma dos números digitados é ${soma}`)
+
+// Para concatenar variáveis dentro de uma string use ${variavel}
