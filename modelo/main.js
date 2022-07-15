6c8a433e0b3fac57c272d1de4349ac1b07058ce5
Eduardo Faria <avspnn@gmail.com>
2022-07-15 16:49:33 -0300

    exercício

---
+++ modelo/main.js
@@ -0,0 +1,15 @@
+function main(){
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (evento) =>{
+        evento.preventDefault()
+
+        const result = document.querySelector('.result')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+
+
+
+    })
+
+} main()
