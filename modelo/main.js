d76dbf76211cd479c1408a951fd7a1f98b1c7313
Eduardo Faria <eduardo.gfaria28@gmail.com>
2022-07-17 17:57:04 -0300

    exercicio realizado

--- modelo/main.js
+++ modelo/main.js
@@ -1,15 +1,35 @@
-function main(){
-    const form = document.querySelector('form')
-    form.addEventListener('submit', (evento) =>{
-        evento.preventDefault()
+function main() {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', evento => {
+    evento.preventDefault()
 
-        const result = document.querySelector('.result')
-        const peso = form.querySelector('.peso')
-        const altura = form.querySelector('.altura')
+    const result = document.querySelector('.result')
+    const peso = form.querySelector('.peso')
+    const altura = form.querySelector('.altura')
 
+    const imc = ((peso, altura) => {
+      return peso / altura ** 2
+    })(Number(peso.value), Number(altura.value))
 
-
-
-    })
-
-} main()
+    function resultado(msg) {
+      result.innerHTML = ''
+      const paragraph = document.createElement('p')
+      paragraph.innerHTML = msg
+      result.appendChild(paragraph)
+    }
+    if (imc < 18.5) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Abaixo do peso.`)
+    } else if (imc >= 18.5 && imc < 25) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Peso normal.`)
+    } else if (imc >= 25 && imc < 30) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Sobrepeso.`)
+    } else if (imc >= 30 && imc < 35) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Obesidade grau 1.`)
+    } else if (imc >= 35 && imc < 40) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Obesidade grau 2.`)
+    } else if (imc >= 40) {
+      resultado(`Seu imc é ${imc.toFixed(2)}. Obesidade grau 3`)
+    }
+  })
+}
+main()
