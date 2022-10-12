7eb9f7da72249f99df1b5fe471df64a64188396b
Eduardo Faria <eduardo.gfaria28@gmail.com>
2022-10-12 12:55:20 -0300

    att

--- módulo7/README.md
+++ módulo7/README.md
@@ -1,1 +1,3 @@
 ## aula80: Criando classes
+
+## aula81: Getters e Setters
---
+++ módulo7/aula81/index.js
@@ -0,0 +1,53 @@
+// o symbol pode ser criado para definir uma propriedade privada na classe
+// o symbol é sempre diferente cada vez que a função é executada
+// Symbol('descrição')
+const _velocidade = Symbol('velocidade')
+
+class Carro {
+  constructor(nome) {
+    this.nome = nome
+    this[_velocidade] = 0
+  }
+  // será executado quando for feito um set do valor dessa propriedade
+  set velocidade(valor) {
+    if (typeof valor != 'number') return
+    if (valor >= 100 || valor <= 0) return
+    this[_velocidade] = valor
+  }
+  // será executado quando for feito um get desse valor
+  get velocidade() {
+    return this[_velocidade]
+  }
+
+  acelerar() {
+    if (this[_velocidade] >= 100) return
+    this[_velocidade]++
+  }
+
+  freiar() {
+    if (this[_velocidade] <= 0) return
+    this[_velocidade]--
+  }
+}
+
+const c1 = new Carro('fusca')
+c1.velocidade = 99 // está dentro da faixa de valores aceitos, então aceita
+
+//==========================================================================
+class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+  }
+
+  get nomeCompleto() {
+    return `${this.nome} ${this.sobrenome}`
+  }
+  // setando o nome completo já seta nome e sobrenome
+  set nomeCompleto(valor) {
+    ;[this.nome, this.sobrenome] = valor.split(' ')
+  }
+}
+
+const p1 = new Pessoa()
+p1.nomeCompleto = 'Eduardo Faria'
